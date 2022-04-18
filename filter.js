e0c72d657745a693e48b888c04777cb9d29f1c7b
abiodunraheem <raheemabiodun23@gmail.com>
2022-04-18 22:30:41 +0100

    Update filter file

--- filter.js
+++ filter.js
@@ -2,4 +2,11 @@
 const filter = document.getElementById('search');
 
 
+filter.addEventListener('keyup', filterItems);
+function filterItems(e){
+// convert text to lowercase
+var text = e.target.value.toLowerCase();
+// Get lis
+
+}
 
