65950672ab22e5c32c7c231d3c3be6e5f90daad8
Bria222 <briantechologies.co.ke@gmail.com>
2022-04-19 00:46:08 +0300

    fixed linters by removing unsused code

--- filter.js
+++ filter.js
@@ -2,9 +2,5 @@
 const filter = document.getElementById('search');
 
 filter.addEventListener('keyup');
-function filterItems(e) {
-// convert text to lowercase
-  // eslint-disable-next-line no-unused-vars
-  const text = e.target.value.toLowerCase();
-  // Get lis
-}
+
+
